29809f189c6ab6b2e1cfd2104a9d0eaacd7d6518
nvernooy <vernooy.n@gmail.com>
2019-11-21 15:58:48 +0200

    #14 user select on dashboards and collections working

--- src/app/collection/collection.dialog.component.ts
+++ src/app/collection/collection.dialog.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, Inject, OnDestroy, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {Collection} from '../_models/collection';
 import {MAT_DIALOG_DATA, MatDialogRef, MatSelect} from '@angular/material';
@@ -12,18 +12,17 @@ import {take, takeUntil} from 'rxjs/operators';
     templateUrl: './collection.dialog.component.html',
     styleUrls: ['./collection.component.css']
 })
-export class CollectionDialogComponent implements OnInit {
-
-    protected users: User[] = [];
-    public userCtrl: FormControl = new FormControl();
-    public filteredUsers: ReplaySubject<User[]> = new ReplaySubject<User[]>(1);
-
-    @ViewChild('userSelect') multiSelect: MatSelect;
-    protected _onDestroy = new Subject<void>();
+export class CollectionDialogComponent implements OnInit, AfterViewInit, OnDestroy {
 
     public form: FormGroup;
     collection: Collection;
     state: string;
+    public userNameFilter: FormControl = new FormControl();
+    public searching = false;
+    public filteredUsers: ReplaySubject<User[]> = new ReplaySubject<User[]>(1);
+    protected _onDestroy = new Subject<void>();
+
+    @ViewChild('userSelect') userSelect: MatSelect;
 
     constructor(
         public dialogRef: MatDialogRef<CollectionDialogComponent>,
@@ -41,47 +40,43 @@ export class CollectionDialogComponent implements OnInit {
             id: this.collection.id,
             title: this.collection.title,
             description: this.collection.description,
-            usersFilter: ''
+            users: [this.collection.users]
         };
         this.form = this.fb.group(formData);
-
-        this.userCtrl.setValue(this.collection.users);
-        this.filteredUsers.next(this.collection.users.slice());
+        this.filteredUsers.next(this.collection.users);
 
         this.onFormChanges();
     }
 
-    onFormChanges(): void {
-        this.form.controls.usersFilter.valueChanges
-            .pipe(takeUntil(this._onDestroy))
-            .subscribe(() => {
-                this.filterUsers();
-            });
+    ngAfterViewInit() {
+        this.setInitialValue();
+    }
 
+    ngOnDestroy() {
+        this._onDestroy.next();
+        this._onDestroy.complete();
+    }
+
+    onFormChanges(): void {
+        this.userNameFilter.valueChanges.subscribe(value => this.filterUsers(value));
     }
 
     protected setInitialValue() {
-        this.filteredUsers.pipe(take(1), takeUntil(this._onDestroy))
-            .subscribe(() => {
-                this.multiSelect.compareWith = (a: User, b: User) => a && b && a.id === b.id;
-            });
+        this.filteredUsers.pipe(take(1), takeUntil(this._onDestroy)).subscribe(() => {
+            this.userSelect.compareWith = (a: User, b: User) => a && b && a.id === b.id;
+        });
     }
 
-    protected filterUsers() {
-        if (!this.users) {
-            return;
-        }
-        // get the search keyword
-        let search = this.form.controls.usersFilter.value;
+    protected filterUsers(search) {
         if (!search) {
-            this.filteredUsers.next(this.users.slice());
+            this.filteredUsers.next(this.form.controls.users.value);
             return;
-        } else {
-            search = search.toLowerCase();
         }
         // filter the users
-        this.userService.getAll({'fullname': search}).subscribe(resp => {
+        this.searching = true;
+        this.userService.getAll({'name': search}).subscribe(resp => {
             this.filteredUsers.next(resp);
+            this.searching = false;
         });
     }
 
