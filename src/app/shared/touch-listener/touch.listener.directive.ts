da52576b8b0053877bbd39c7d9f69fa1f03f551e
rochecompaan <roche@upfrontsoftware.co.za>
2020-02-26 09:33:20 +0200

    Alias event to fix reference error in Edge, FF and Safari. Closes #52

--- src/app/shared/touch-listener/touch.listener.directive.ts
+++ src/app/shared/touch-listener/touch.listener.directive.ts
@@ -1,16 +1,19 @@
 import {Directive, HostListener} from '@angular/core';
 import {Subject} from 'rxjs';
 
+// Alias event to work around Edge, FF and Safari reference error
+export type AppTouchEvent = TouchEvent;
+
 @Directive({
     selector: '[appTouchListener]'
 })
 export class TouchListenerDirective {
 
-    private touchSub = new Subject<TouchEvent>();
+    private touchSub = new Subject<AppTouchEvent>();
     public touch = this.touchSub.asObservable();
 
     @HostListener('touchmove', ['$event'])
-    onTouchMove(event: TouchEvent) {
+    onTouchMove(event: AppTouchEvent) {
         this.touchSub.next(event);
     }
 }
