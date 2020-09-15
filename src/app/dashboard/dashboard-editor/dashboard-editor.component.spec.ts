1d394df19c491820f9f886d1f6696a1041a0cf89
nvernooy <vernooy.n@gmail.com>
2020-09-15 16:41:25 +0200

    Update unit tests

--- src/app/dashboard/dashboard-editor/dashboard-editor.component.spec.ts
+++ src/app/dashboard/dashboard-editor/dashboard-editor.component.spec.ts
@@ -1,25 +1,63 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { DashboardEditorComponent } from './dashboard-editor.component';
+import {DashboardEditorComponent} from './dashboard-editor.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule} from '../../material.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from '@angular/core';
+import {Dashboard} from '../../_models/dashboard';
+import {AuthenticationService} from '../../_services/authentication.service';
+import {UserService} from '../../_services/user.service';
+import {of} from 'rxjs';
+import {DashboardService} from '../../_services/dashboard.service';
+
+
+export class AuthenticationServiceStub {
+    hasRole(role) {
+        return true;
+    }
+}
+
+export class UserServiceStub {
+    getAll(filter) {
+        return of([]);
+    }
+}
+
+export class DashboardServiceStub {
+    edit(form) {
+        return of(form);
+    }
+}
+
 
 describe('DashboardEditorComponent', () => {
-  let component: DashboardEditorComponent;
-  let fixture: ComponentFixture<DashboardEditorComponent>;
+    let component: DashboardEditorComponent;
+    let fixture: ComponentFixture<DashboardEditorComponent>;
+    const dashboard = new Dashboard();
+    dashboard.users = [];
+    dashboard.title = 'Title';
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DashboardEditorComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [DashboardEditorComponent],
+            imports: [FormsModule, ReactiveFormsModule, MaterialModule, BrowserAnimationsModule],
+            providers: [{provide: AuthenticationService, useClass: AuthenticationServiceStub},
+                {provide: UserService, useClass: UserServiceStub},
+                {provide: DashboardService, useClass: DashboardServiceStub}],
+            schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardEditorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DashboardEditorComponent);
+        component = fixture.componentInstance;
+        component.dashboard = dashboard;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/dashboard/variable/variable-dialog.component.spec.ts
+++ src/app/dashboard/variable/variable-dialog.component.spec.ts
@@ -1,25 +1,40 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { VariableDialogComponent } from './variable-dialog.component';
+import {VariableDialogComponent} from './variable-dialog.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing';
+import {MaterialModule} from '../../material.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 
 describe('VariableDialogComponent', () => {
-  let component: VariableDialogComponent;
-  let fixture: ComponentFixture<VariableDialogComponent>;
+    let component: VariableDialogComponent;
+    let fixture: ComponentFixture<VariableDialogComponent>;
+    const variable = {};
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ VariableDialogComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [VariableDialogComponent],
+            imports: [FormsModule, ReactiveFormsModule, RouterTestingModule, MaterialModule, BrowserAnimationsModule,
+                HttpClientTestingModule],
+            schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+            providers: [
+                {provide: MatDialogRef, useValue: {}},
+                {provide: MAT_DIALOG_DATA, useValue: {variable}}
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(VariableDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(VariableDialogComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
